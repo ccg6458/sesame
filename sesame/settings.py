1b116cbac5303415b2131516c6a7e38d93e422be
chenchenggui <chenchenggui@worken.cn>
2021-04-27 15:31:01 +0800

    update

--- sesame/settings.py
+++ sesame/settings.py
@@ -133,7 +133,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-SECRET_KEY = 'QRtQyR!VBygN6aZTcevqu61V'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
