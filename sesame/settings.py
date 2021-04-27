cb4161acb776683107759a84fdfcff227233d5d1
chenchenggui <chenchenggui@worken.cn>
2021-04-27 10:40:19 +0800

    update

--- sesame/settings.py
+++ sesame/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 SECRET_KEY = 'QRtQyR!VBygN6aZTcevqu61V'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
